6577ddbaf6a4532697b0d7ee8516a53acbc28631
AniaGoodness <aniagoodness@gmail.com>
2023-03-14 20:59:45 +0100

    task3 imageGallery

--- js/task-03.js
+++ js/task-03.js
@@ -14,12 +14,8 @@ const images = [
 ];
 
 const galleryList = document.querySelector(".gallery");
-const insertGallery = `<li><img src= "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260.jpg" 
-    alt= "White and Black Long Fur Cat" ></li> <li> <img  src= "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260.jpg"
-    alt= "Orange and White Koi Fish Near Yellow Koi Fish"> </li> <li><img src= "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260.jpg"
-    alt= "Group of Horses Running"></li>`; 
-//const insertGallery = images.map((image) => `<li><img ${images.url} ></li>`);
-galleryList.insertAdjacentHTML("afterbegin", insertGallery);
+const insertGallery = images.map(image => `<li><img src= ${image.url} alt= "${image.alt}"></li>`).join("");
+galleryList.insertAdjacentHTML("beforeend", insertGallery);
 
 galleryList.style.listStyle = "none";
 galleryList.style.display = "flex";
